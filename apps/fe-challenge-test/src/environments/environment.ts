a5567345ee0852e02639ecf0a96e38fd31285539
Lorenzo Lopez <lorenzolopez200123@gmail.com>
2024-09-05 01:16:33 -0300

    add API_URL RENDER DEPLOY

--- apps/fe-challenge-test/src/environments/environment.ts
+++ apps/fe-challenge-test/src/environments/environment.ts
@@ -1,22 +1,3 @@
-import { PLATFORM_ID, inject } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
-
-declare const process: any; // This allows us to use process.env without TypeScript errors
-
 export const environment = {
-  get API_URL(): string {
-    const platformId = inject(PLATFORM_ID);
-
-    if (isPlatformBrowser(platformId)) {
-      // Client-side
-      return (
-        (window as any)['env']?.['ROOT_API_URL'] || 'http://localhost:3000'
-      );
-    } else {
-      // Server-side
-      const renderExternalHostname =
-        process.env['RENDER_EXTERNAL_HOSTNAME'] || 'localhost:3000';
-      return `https://${renderExternalHostname}-backend.onrender.com`;
-    }
-  },
+  API_URL: 'https://backend-3grl.onrender.com',
 };
